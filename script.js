506079954005b37d1e8f5c2638b154ec23bed236
Caroline <carolinefernandesc@hotmail.com>
2022-11-04 18:37:43 -0300

    desafio 04 corrigindo lógica

--- script.js
+++ script.js
@@ -2,17 +2,29 @@ const corPreta = document.querySelector('#corPreta');
 const cor2 = document.querySelector('#cor2');
 const cor3 = document.querySelector('#cor3');
 const cor4 = document.querySelector('#cor4');
-const botaoPRandomColor = document.querySelector('#p-random-color');
+const botaoRandomColor = document.querySelector('#button-random-color');
+const classColors = document.getElementsByClassName('color');
 
-function mudandoDeCor(cor = 1) {
+function corAleatoria(cor = 1) {
     const r = Math.random() * 255;
     const g = Math.random() * 255;
     const b = Math.random() * 255;
   
     return `rgba(${r}, ${g}, ${b}, ${cor})`;
-}
+};
 
 corPreta.style.backgroundColor = 'black';
-cor2.style.backgroundColor = mudandoDeCor();
-cor3.style.backgroundColor = mudandoDeCor();
-cor4.style.backgroundColor = mudandoDeCor();
+cor2.style.backgroundColor = corAleatoria();
+cor3.style.backgroundColor = corAleatoria();
+cor4.style.backgroundColor = corAleatoria();
+
+botaoRandomColor.addEventListener('click', () => {
+    const novaCor = document.getElementsByClassName('color');
+    for (let index in novaCor){
+        if (index === '0'){
+            novaCor[index].style.backgroundColor = 'black';
+        } else {
+            novaCor[index].style.backgroundColor = corAleatoria();
+        }
+    }
+});

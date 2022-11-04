fe71bdf2e5d9d66b327bc2eee95838dda0ffbb68
Caroline <carolinefernandesc@hotmail.com>
2022-11-04 15:27:06 -0300

    desafio 02

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const corPreta = document.querySelector('#corPreta');
+const cor2 = document.querySelector('#cor2');
+const cor3 = document.querySelector('#cor3');
+const cor4 = document.querySelector('#cor4');

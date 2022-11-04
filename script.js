e3e6d6656ae7b6447fb87fa934e43a7fbb503ea6
Caroline <carolinefernandesc@hotmail.com>
2022-11-04 15:03:58 -0300

    desafio 01 refazendo

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const header = document.createElement('header');
-document.body.appendChild(header);
 
-const h1 = document.createElement('h1');
-h1.id = 'title';
-h1.innerText = 'Paleta de Cores';
-header.appendChild(h1);
